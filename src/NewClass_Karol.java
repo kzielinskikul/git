e4706fb6003a0856762eb48be36e350c8ebca3d5
KaDeEm21 <kadeem393@gmail.com>
2023-05-26 20:47:53 +0200

    Add methods: Test1(), Test2()

--- src/NewClass_Karol.java
+++ src/NewClass_Karol.java
@@ -2,4 +2,12 @@ public class NewClass_Karol {
     public void Test(){
         System.out.println("Hello!");
     }
+
+    public void Test1(String message) {
+        System.out.println(message);
+    }
+
+    public void Test2(String message) {
+        System.out.println(message+" :)");
+    }
 }
