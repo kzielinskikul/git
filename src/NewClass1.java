f6cc1294fbfdff04b3474cce1e78c7b650b44ab9
Lavieers <kinga45633@wp.pl>
2023-05-26 20:49:02 +0200

    Add NewClass1.java

--- src/NewClass1.java
+++ src/NewClass1.java
@@ -2,4 +2,10 @@ public class NewClass1 {
     public static void main(String[] args) {
         System.out.println("New Class");
     }
+    public void test() {
+        System.out.println("Test0");
+    }
+    public void test1() {
+        System.out.println("Test1");
+    }
 }
